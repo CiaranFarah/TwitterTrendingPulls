8dae8216b5f72e59ce635d14e436af9cbd4b48a5
CiaranFarah <51178150+CiaranFarah@users.noreply.github.com>
2021-08-27 01:36:01 -0400

    Security Update
    Took out hardcoded Twitter Developer Tokens and DB connection for security purposes before making this project public.

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from TwitterPull import twitter_pull
 from DailySnapshot import daily_snapshot
 from sqlalchemy import create_engine
 
-CANADA_WOEID = 23424775
-c_key = "Qx9NUC17dFkPK3i6862cnRJRg"
-c_secret = "r4nGh2tXsnr16WZDmDEeOxk5IZsvnDLnCpUJL0i34uVDvylhE2"
-a_token = "1430584802407813120-1DHy1xPPyQvKr63Ue7y3I4o347Hmt2"
-a_token_secret = "HTG9I79oBziej1K2NookkDiOEQ7WT1YLpUVGwhkI4Ry5H"
+passwords_file = open("passwords.txt")
+passwords = {}
+# Get all info from passwords text file for security purposes
+for line in passwords_file:
+    equals_index = line.index("=")
+    passwords[line[:equals_index]] = line[equals_index + 1:].strip()
 
-trending_topics_df = twitter_pull(c_key, c_secret, a_token, a_token_secret)
+passwords_file.close()
 
-engine = create_engine("postgresql://postgres:Pokemon11!@localhost:5432/TwitterPulls")  # Using local PostgreSQL DB
+trending_topics_df = twitter_pull(passwords["c_key"], passwords["c_secret"], passwords["a_token"], passwords["a_token_secret"])
+
+engine = create_engine(passwords["postgres"])  # Using local PostgreSQL DB
 trending_topics_df.to_sql("TrendingTopics", engine, if_exists="append", index=False)
 
 daily_trending_snapshot_df = daily_snapshot()
